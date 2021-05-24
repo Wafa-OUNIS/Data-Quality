344431d3d821c38aac287121607d8d65a7a4bea3
wafa ounis <wafa.ounis@ensi-uma.tn>
2021-05-24 17:23:19 +0200

    Uploading project

---
+++ QD_PROJET/poms/jobs/process/detectioncompletude_0.1/src/main/templates/Job_run_template.bat
@@ -0,0 +1,3 @@
+%~d0
+cd %~dp0
+java -Dtalend.component.manager.m2.repository="%cd%/../lib" -Xms256M -Xmx1024M -Dfile.encoding=UTF-8 -cp .;../lib/routines.jar;../lib/crypto-utils.jar;../lib/dom4j-1.6.1.jar;../lib/jaxen-1.1.1.jar;../lib/jxl.jar;../lib/log4j-1.2.17.jar;../lib/talend_file_enhanced_20070724.jar;../lib/talendcsv.jar;../lib/xpathutil-1.0.0.jar;detectioncompletude_0_1.jar; qd_projet.detectioncompletude_0_1.DetectionCompletude  %*
